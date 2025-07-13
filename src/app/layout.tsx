f783998703ffae6e4512088369a180d3ffd0a471
wesley <wesleylwu@gmail.com>
2025-07-12 21:16:21 -0700

    add images, fonts, and color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Quicksand } from "next/font/google";
 import { ReactQueryClientProvider } from "@/utils/react-query";
 
-const inter = Inter({ subsets: ["latin"] });
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--quicksand-font",
+});
 
 export const metadata = {
   title: "Wesley Wu's Portfolio",
-  description: "A website showcasing Wesley Wu's experiences, projects, and skills.",
+  description:
+    "A website showcasing Wesley Wu's experiences, projects, and skills.",
 };
 
 type LayoutProps = {
@@ -16,7 +22,7 @@ type LayoutProps = {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={quicksand.className}>
         <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
       </body>
     </html>
